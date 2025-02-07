35902f48761b5206b7346d61df3a70f752c6417c
Vikas Joshi <145147066+vicky262600@users.noreply.github.com>
2025-02-07 12:03:58 -0500

    completed 70% of this porject

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:[
+            "images.paxels.com", "images.pexels.com",
+
+        ]
+    }
+};
 
 export default nextConfig;
